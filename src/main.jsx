96d4f9fe3171ac272e3bfcccdf127998cf2013da
Rodrigo Rossi <rodrigoliveira.rossi@gmail.com>
2026-06-03 16:42:42 -0300

    feat: register /agendamento_online route and configure router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import OnlineBooking from './components/OnlineBooking/OnlineBooking.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/agendamento_online" element={<OnlineBooking />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
